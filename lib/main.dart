cca240b6a2b3af3ba805965dbdb9a8364485c332
RNV <RNV@d.c>
2023-08-06 19:03:42 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const CryptoCurrenciesListApp());
+}
+
+class CryptoCurrenciesListApp extends StatelessWidget {
+  const CryptoCurrenciesListApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'CryptoCurrenciesList',
+      theme: ThemeData(
+       scaffoldBackgroundColor: const Color.fromARGB(255, 31, 31, 31),
+        primarySwatch: Colors.yellow,
+        textTheme: TextTheme(
+          
+        ),
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('CryptoCurrenciesList'),
+      ),
+      body: ListView.builder(
+        itemCount: 10,
+          itemBuilder: (context, i) => ListTile(),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
